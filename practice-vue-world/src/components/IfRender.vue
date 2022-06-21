3997302e09ae0433a7dcea0d647f40c79132d2d3
poeynus <poeynus@gmail.com>
2022-06-21 17:48:11 +0900

    vue3 예제 코드 작성 및 연습  22.06.20

---
+++ practice-vue-world/src/components/IfRender.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{ counter }}</p>
+  <p v-if="counter < 5">5보다 작습니다.</p>
+  <p v-else>5와 같거나 큽니다.</p>
+  <button @click="counter++">숫자 상승</button>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let counter = ref(0);
+    return { counter };
+  },
+};
+</script>
---
+++ practice-vue-world/src/components/UserCounter.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>{{ counter + counter2 }}</p>
+  <button @click="counter++">숫자 상승</button>
+  <button @click="onClick">숫자 상승</button>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  data() {
+    return {
+      counter2: 0,
+    };
+  },
+  setup() {
+    let counter = ref(0);
+
+    const onClick = (evt) => {
+      if (evt) {
+        evt.preventDefault();
+        counter.value++;
+      }
+    };
+    return {
+      counter,
+      onClick,
+    };
+  },
+  methods: {
+    onClick2: function (evt) {
+      if (evt) {
+        evt.preventDefault();
+      }
+      this.counter2++;
+    },
+  },
+};
+</script>
